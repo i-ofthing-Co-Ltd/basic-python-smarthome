5e25b51222d3b778c3c8268391b69a59aab6afb8
massatronp <massatron1@gmail.com>
2022-02-03 10:38:11 +0700

    Update env.py

--- env.py
+++ env.py
@@ -1,7 +1,8 @@
 # online
 ACCESS_ID = "395t8a9gb55foo7ehez5"
 ACCESS_KEY = "3d0220cc74c744d982f957f0f7e6f3f0"
+ENDPOINT = "https://openapi.tuyaus.com"
 USERNAME = ""
 PASSWORD = ""
 ASSET_ID = ""
-ENDPOINT = "https://openapi.tuyaus.com"
+
